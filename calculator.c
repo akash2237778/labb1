1008fbbadb8760c1b39e2053fa36e3b1dbe9669b
Akash <akash2237778@gmail.com>
2019-01-16 13:17:12 +0530

    main is added

--- calculator.c
+++ calculator.c
@@ -2,9 +2,20 @@
 using namespace std;
 class calculator
 {
-int add (int,int);
+int add(int,int);
 int divide(int,int);
-void substract(int,int);
+int substract(int,int);
+int multiply(int,int);
 public:
 int op1,op2;
-};
+}A;
+void main()
+{
+cout<<"enter the to numbers";
+cin>>op1;
+cin>>op2;
+A.add(op1,op2);
+A.divide(op1,op2);
+A.substract(op1,op2);
+A.multiply(op1,op2);
+}

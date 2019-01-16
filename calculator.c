66767bd549023447144eef90ec0b4379e35dce65
Akash <akash2237778@gmail.com>
2019-01-16 21:23:29 +0530

    correction_1

--- calculator.c
+++ calculator.c
@@ -38,4 +38,44 @@ int multiply(op1,op2)
 {
 cout<<"multiply"<<endl;
 cout<<(op1*op2)<<endl;
+}[200~#include<iostream>
+using namespace std;
+class calculator
+{
+	int add(int,int);
+	int divide(int,int);
+	int substract(int,int);
+	int multiply(int,int);
+	public:
+	int op1,op2;
+}A;
+void main()
+{
+	cout<<"enter the to numbers";
+	cin>>op1;
+	cin>>op2;
+	A.add(op1,op2);
+	A.divide(op1,op2);
+	A.substract(op1,op2);
+	A.multiply(op1,op2);
+}
+int add(op1,op2)
+{
+	cout<<"add"<<endl;
+	cout<<(op1+op2)<<endl;
+}
+int divide(op1,op2)
+{
+	cout<<"divide"<<endl;
+	cout<<(op1/op2)<<endl;
+}
+int substract(op1,op2)
+{
+	cout<<"substract"<<endl;
+	cout<<(op1-op2)<<endl;
+}
+int multiply(op1,op2)
+{
+	cout<<"multiply"<<endl;
+	cout<<(op1*op2)<<endl;
 }

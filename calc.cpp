81b763030be0efb88e6d3ab2eff9895abd0c32ca
akash2237778 <akash2237778@gmail.com>
2019-02-19 14:31:44 -0800

    defining class having operations

---
+++ calc.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class calculator
+{
+private:
+     int a,b;
+public:
+int add(int a,int b)
+{
+cout<<"a+b :"<<a+b;
+}
+int divide(int a,int b)
+{
+cout<<"a/b :"<<a/b;
+}
+int substract(int a,int b)
+{
+cout<<"a-b :"<<a-b;
+}
+int multiply(int a,int b)
+{
+cout<<"a*b :"<<a*b;
+}
+}
